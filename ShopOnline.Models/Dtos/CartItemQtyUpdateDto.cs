646328aea70f191feccd9c029e610ddcf4ee12be
DEBASIS PAUL®™ (Zhýralý Pāvils) #BlackLivesStillMatter <DebasisPaul@users.noreply.github.com>
2022-06-10 01:18:28 -0700

    Updated On June10

---
+++ ShopOnline.Models/Dtos/CartItemQtyUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnline.Models.Dtos
+{
+    internal class CartItemQtyUpdateDto
+    {
+        public int CartItemId { get; set; }
+
+        public int Qty { get; set; }
+
+
+    }
+}
